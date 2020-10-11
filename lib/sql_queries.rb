03771195f147b3eb6f661b9c24e316f840b65aa2
conner fortner <fortner.conner@yahoo.com>
2020-10-11 21:02:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,12 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
- "SELECT users.name, users.age, SUM(pledges.amount)
-  FROM users
-  INNER JOIN pledges
-  ON users.id = pledges.user_id
-  GROUP BY users.name
-  ORDER BY users.name;"
+ "SELECT Users.name, Users.age, SUM(Pledges.amount) FROM pledges INNER JOIN Users ON Pledges.user_id = Users.id GROUP BY user_id ORDER BY(Users.name);"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
