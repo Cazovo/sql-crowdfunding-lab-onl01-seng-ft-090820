6527eb3588394d511842192a9fe8a341dc7cd38b
conner fortner <fortner.conner@yahoo.com>
2020-10-11 21:17:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,22 +1,22 @@
   
 CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date TEXT,
-  end_date TEXT
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal REAL,
+    start_date INTEGER,
+    end_date INTEGER
 );
 
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
 );
 
 CREATE TABLE pledges (
-  id INTEGER PRIMARY KEY,
-  amount INTEGER,
-  user_id INTEGER,
-  project_id INTEGER
+    id INTEGER PRIMARY KEY,
+    amount REAL,
+    user_id INTEGER,
+    project_id INTEGER
 );
